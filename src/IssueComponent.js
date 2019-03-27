88cb48b43c9ad79a3cc08253cdeacc7d49d23085
Isaac <isaacli911@gmail.com>
2019-03-27 10:23:04 -0400

    fixed author urls

--- src/IssueComponent.js
+++ src/IssueComponent.js
@@ -13,7 +13,7 @@ class IssueComponent extends React.Component {
         {this.props.flag === 'open' ? open : close}
         <a href={this.props.url} target="_blank" className="issue-text title" rel="noopener noreferrer">{this.props.title}</a>
         <p className="issue-text issues-snippet">{this.props.snippet}</p>
-        <p className="issue-text">Created on {this.props.date} by <a href={this.props.author.html_url} className="author-link" href="_blank" rel="noopener noreferrer">{this.props.author.login}</a> <img src={this.props.author.avatar_url} className="avatar-pic" alt="avatar-pic"/></p>
+        <p className="issue-text">Created on {this.props.date} by <a href={this.props.author.html_url} className="author-link" target="_blank" rel="noopener noreferrer">{this.props.author.login}</a> <img src={this.props.author.avatar_url} className="avatar-pic" alt="avatar-pic"/></p>
       </div>
     )
   }
