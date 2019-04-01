0a98535b2182d80c5522d7fa75c0384c1c13ae24
Lucas Marcelli <lucas.marcelli@we.org>
2019-03-31 20:18:36 -0400

    Updates

---
+++ precache-manifest.dd6f15046fa6b47871f5205adf126f14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2059dc5594b239a3135e",
+    "url": "/react_github_issues/static/css/main.f5102a95.chunk.css"
+  },
+  {
+    "revision": "2059dc5594b239a3135e",
+    "url": "/react_github_issues/static/js/main.0dc6969a.chunk.js"
+  },
+  {
+    "revision": "a3e3c2699f84e89a7f24",
+    "url": "/react_github_issues/static/js/runtime~main.5004039e.js"
+  },
+  {
+    "revision": "5441c3a8e6cae9cf50bd",
+    "url": "/react_github_issues/static/js/2.36797133.chunk.js"
+  },
+  {
+    "revision": "8e9b0bccac4248a00c02d15f44038a44",
+    "url": "/react_github_issues/index.html"
+  }
+];
