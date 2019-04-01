ac5ccd027d6fe295934b9b45af5b7404e4000ab5
Lucas Marcelli <lucas.marcelli@we.org>
2019-03-31 20:02:52 -0400

    Docs

---
+++ docs/precache-manifest.ac8e7c03554a1e303a0840561a7beaa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5b17e5e31722f0bf9ee",
+    "url": "/static/css/main.f5102a95.chunk.css"
+  },
+  {
+    "revision": "a5b17e5e31722f0bf9ee",
+    "url": "/static/js/main.9342f5b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c0230e82d4d7272c5ab5",
+    "url": "/static/js/2.2595b05e.chunk.js"
+  },
+  {
+    "revision": "50b276384533e01b5d1dafeb067048f5",
+    "url": "/index.html"
+  }
+];
